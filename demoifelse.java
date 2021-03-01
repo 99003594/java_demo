6b8a65e0bc76c46a3d2371fdb3d98dd919947ece
99003594 <78541986+99003594@users.noreply.github.com>
2021-03-01 16:07:17 +0530

    Add files via upload

---
+++ demoifelse.java
@@ -0,0 +1,15 @@
+package test_demo;
+
+public class demoifelse {
+	public static void main(String args[]) {
+	int age=20;
+	if(age>=10) {
+		System.out.println("a is greater than 10");
+	}
+	else
+	{
+		System.out.println("a is less than 10");
+	}
+}
+
+}
---
+++ demowhileloop.java
@@ -0,0 +1,14 @@
+package test_demo;
+
+public class demowhileloop 
+{
+	public static void main(String args[]) 
+	{
+		int a=10;
+		while (a<20)
+		{
+			System.out.println(a);
+		  a++;
+		}
+	}
+}
