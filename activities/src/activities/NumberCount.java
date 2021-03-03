5e89fe480a3ea5c000837446f2382d51c929146f
99003594 <78541986+99003594@users.noreply.github.com>
2021-03-03 09:08:29 +0530

    Add files via upload

---
+++ activities/src/activities/NumberCount.java
@@ -0,0 +1,38 @@
+package activities;
+
+import java.util.Scanner;
+
+public class NumberCount {
+	static int count;
+	public static int findElementCount(int a[],int num,int n){
+		for(int i=0;i<num;i++){
+	    if(a[i] == n)
+		{
+		count++;
+		}
+		}
+		return count;
+		}
+		public static void main(String[] args){
+		int n;
+		int search;
+		int result;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++){
+		arr[i] = sc.nextInt();
+		}
+		if(n>0)
+		{
+			search = sc.nextInt();
+		result = findElementCount(arr,n,search);
+		System.out.println(result);
+		}
+		else
+		{
+			System.out.println("Invalid Input");
+		}
+		}
+}
