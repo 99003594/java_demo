188d887e7963f57454d59681f7acaa8b6c059805
99003594 <78541986+99003594@users.noreply.github.com>
2021-03-05 09:08:51 +0530

    Add files via upload

---
+++ string_and_stringbuffer/src/stringbuffer/StringBufferCapacity.java
@@ -0,0 +1,9 @@
+package stringbuffer;
+
+public class StringBufferCapacity {
+	 public static void main(String[] args) {  
+	        StringBuffer sb = new StringBuffer();  
+	        //printing default capacity of string buffer  
+	        System.out.println("default capacity: " + sb.capacity());      
+	    }  
+}
